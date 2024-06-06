dbbbcbf09af1fb9c52b7d3fb44b23d6afbea1d83
Carlo Astudillo <carloasto11@gmail.com>
2024-06-06 16:55:18 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppCheck.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
