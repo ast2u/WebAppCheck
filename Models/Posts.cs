a3c4ad2d7006f92a9963af175ce4708217b11f5c
Carlo Astudillo <carloasto11@gmail.com>
2024-06-24 18:02:54 +0800

    new  changess design

--- Models/Posts.cs
+++ Models/Posts.cs
@@ -9,11 +9,26 @@ namespace WebAppCheck.Models
         [Key]
         public int PostId { get; set; }
 
+        private string _postTitle;
+
         [Column(TypeName = "nvarchar(50)")]
         [Required(ErrorMessage = "You need a title for your Post.")]
         [DisplayName("Post Title")]
         [MaxLength(50, ErrorMessage = "Maximum 50 characters only.")]
-        public string PostTitle { get; set; }
+        public string PostTitle {
+            get => _postTitle;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _postTitle = char.ToUpper(value[0]) + value.Substring(1);
+                }
+                else
+                {
+                    _postTitle = value;
+                }
+            }
+        }
 
         [Column(TypeName = "nvarchar(300)")]
         [DisplayName("Post Content")]
